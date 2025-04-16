7ec7116eb991325594b00652008a3f2c6f1457c3
JanardanPethani <jpethani11@gmail.com>
2025-04-16 11:25:19 +0530

    Add metadata changes for blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,42 +1,58 @@
-"use client";
-
-import { useParams } from "next/navigation";
-import Head from "next/head";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import BlogLayout from "@/components/Blog/BlogLayout";
 import BlogContainer from "@/components/Blog/BlogContainer";
 import { blogData } from "@/lib/blog-data";
 
-const BlogPage = () => {
-  const { slug } = useParams();
-  const blog = blogData.find(post => post.slug === slug);
-  if (!blog) return <div>Blog not found.</div>;
-
-  return (
-    <>
-      <Head>
-        <title>{blog.title} | My Blog</title>
-        <meta name="description" content={`Read about ${blog.title}`} />
-        <meta name="keywords" content={blog.categories.join(", ")} />
-        <meta property="og:title" content={blog.title} />
-        <meta property="og:description" content={`Read about ${blog.title}`} />
-        <meta property="og:type" content="article" />
-        {/* <meta
-          property="og:url"
-          content={`https://yourwebsite.com/blog/${blog.slug}`}
-        /> */}
-        <meta name="twitter:title" content={blog.title} />
-        <meta name="twitter:description" content={`Read about ${blog.title}`} />
-      </Head>
-      <BlogContainer>
-        <BlogLayout
-          title={blog.title}
-          publishDate={blog.publishDate}
-          categories={blog.categories}
-          content={blog.content}
-        />
-      </BlogContainer>
-    </>
-  );
+type Props = {
+  params: { slug: string };
 };
 
-export default BlogPage;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const blog = blogData.find((post) => post.slug === params.slug);
+
+  if (!blog) {
+    return {
+      title: "Blog Not Found",
+      description: "The requested blog post could not be found.",
+    };
+  }
+
+  return {
+    title: `${blog.title} | My Blog`,
+    description: blog.excerpt || `Read about ${blog.title}`,
+    keywords: blog.categories.join(", "),
+    openGraph: {
+      title: blog.title,
+      description: blog.excerpt || `Read about ${blog.title}`,
+      type: "article",
+      publishedTime: blog.publishDate,
+      authors: ["Janardan Pethani"],
+      // url: `https://yourwebsite.com/blog/${blog.slug}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: blog.title,
+      description: blog.excerpt || `Read about ${blog.title}`,
+    },
+  };
+}
+
+export default function BlogPage({ params }: Props) {
+  const blog = blogData.find((post) => post.slug === params.slug);
+
+  if (!blog) {
+    notFound();
+  }
+
+  return (
+    <BlogContainer>
+      <BlogLayout
+        title={blog.title}
+        publishDate={blog.publishDate}
+        categories={blog.categories}
+        content={blog.content}
+      />
+    </BlogContainer>
+  );
+}
